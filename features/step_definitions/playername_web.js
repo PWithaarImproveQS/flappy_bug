e8e7d1b90474c7d6e41bc8e9be25a25e12cff310
Pieter Withaar <pieter.withaar@improveqs.nl>
2017-01-22 17:37:27 +0000

    webdriver fix

--- features/step_definitions/playername_web.js
+++ features/step_definitions/playername_web.js
@@ -1,69 +1,69 @@
-var {defineSupportCode} = require('cucumber');
- var Assert = require('assert');
+// var {defineSupportCode} = require('cucumber');
+//  var Assert = require('assert');
 
- //const wdio = require('wdio');
+//  //const wdio = require('wdio');
 
-var webdriverio = require('webdriverio');
-var options = { desiredCapabilities: { browserName: 'chrome' } };
-var client = webdriverio.remote(options);
-client
-    .init()
-    .url('https://duckduckgo.com/')
-    .setValue('#search_form_input_homepage', 'WebdriverIO')
-    .click('#search_button_homepage')
-    .getTitle().then(function(title) {
-        console.log('Title is: ' + title);
-        // outputs:
-        // "Title is: WebdriverIO (Software) at DuckDuckGo"
-    })
-    .end();
+// var webdriverio = require('webdriverio');
+// var options = { desiredCapabilities: { browserName: 'chrome' } };
+// var client = webdriverio.remote(options);
+// client
+//     .init()
+//     .url('https://duckduckgo.com/')
+//     .setValue('#search_form_input_homepage', 'WebdriverIO')
+//     .click('#search_button_homepage')
+//     .getTitle().then(function(title) {
+//         console.log('Title is: ' + title);
+//         // outputs:
+//         // "Title is: WebdriverIO (Software) at DuckDuckGo"
+//     })
+//     .end();
     
-defineSupportCode(function({Before, After, Given, When, Then}) {
+// defineSupportCode(function({Before, After, Given, When, Then}) {
 
-    // var browser = wdio.getBrowser({
-    //     desiredCapabilities: {
-    //         browserName: 'chrome'
-    //     }
-    // });
+//     // var browser = wdio.getBrowser({
+//     //     desiredCapabilities: {
+//     //         browserName: 'chrome'
+//     //     }
+//     // });
     
-    // // Initialize selenium standalone server if it is not started yet 
-    // Before({tags: "@webdriver"}, wdio.initSelenium);
+//     // // Initialize selenium standalone server if it is not started yet 
+//     // Before({tags: "@webdriver"}, wdio.initSelenium);
     
-    // // Initialize selenium standalone server if it is not started yet 
-    // Before({tags: "@webdriver"}, function(done) {
-    //   wdio.wrap(function(done)
-    //   {
-    //       console.log("init");
-    //       browser.init();
-    //       console.log(wdio.getBrowser());
-    //   });
-    // });
+//     // // Initialize selenium standalone server if it is not started yet 
+//     // Before({tags: "@webdriver"}, function(done) {
+//     //   wdio.wrap(function(done)
+//     //   {
+//     //       console.log("init");
+//     //       browser.init();
+//     //       console.log(wdio.getBrowser());
+//     //   });
+//     // });
     
-    //   // Initialize selenium standalone server if it is not started yet 
-    // After({tags: "@webdriver"}, function(done) {
-    //   wdio.wrap(function(done)
-    //   {
-    //       browser.end();
-    //   });
-    // });
+//     //   // Initialize selenium standalone server if it is not started yet 
+//     // After({tags: "@webdriver"}, function(done) {
+//     //   wdio.wrap(function(done)
+//     //   {
+//     //       browser.end();
+//     //   });
+//     // });
     
-     Given('a started server', {timeout: 60 * 100}, function (done) {
-         client.init();
-         client.end();
-         done();
-        //wdio.wrap(function (done) {
-            //browser.url('http://www.google.com');
-            //console.log(browser.getTitle);
-            //assert.equal('Google', browser.getTitle());
-        //});
-     });
+//      Given('a started server', {timeout: 60 * 100}, function (done) {
+//          client.init();
+//          client.end();
+//          done();
+//         //wdio.wrap(function (done) {
+//             //browser.url('http://www.google.com');
+//             //console.log(browser.getTitle);
+//             //assert.equal('Google', browser.getTitle());
+//         //});
+//      });
      
-     Given('there already is a player called Pieter in the game', function (done) {
+//      Given('there already is a player called Pieter in the game', function (done) {
        
-       done();
-     });
+//       done();
+//      });
    
-     When('I go to the flappy bug url in my web browser', function (done) {
-       done();
-     });
-});
+//      When('I go to the flappy bug url in my web browser', function (done) {
+//       done();
+//      });
+// });
