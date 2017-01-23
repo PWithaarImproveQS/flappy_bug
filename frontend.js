16017e417cdb31c46f7bdc6bdbf1a4e2dc046716
root <pieter.withaar@improveqs.nl>
2017-01-23 08:02:21 +0000

    Made it possible to run multiple servers

--- frontend.js
+++ frontend.js
@@ -39,7 +39,7 @@ module.exports = {
     	});
     	
     	return http.createServer(app).listen(app.get('port'), function(){
-    	  console.log('Express server listening on port ' + app.get('port'));
+    	  console.log('Flappy bug server listening on port ' + app.get('port'));
     	});
     	
     },
--- server.js
+++ server.js
@@ -5,4 +5,10 @@ var	Const   = require('./sharedConstants').constant,
 	frontend     = require('./frontend.js'),
 	game 	= require('./game_files/game');
 
+
+// game.startServer(frontend.SetupApplicationAndCreateServer(9001));
+// game.startServer(frontend.SetupApplicationAndCreateServer(9002));
+// game.startServer(frontend.SetupApplicationAndCreateServer(9003));
+// game.startServer(frontend.SetupApplicationAndCreateServer(9004));
+// game.startServer(frontend.SetupApplicationAndCreateServer(9005));
 game.startServer(frontend.SetupApplicationAndCreateServer(Const.SERVER_PORT));
